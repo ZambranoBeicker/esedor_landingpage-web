5d1c1010af17cf4c2346207aa1edd1faa92185c4
Beicker Zambrano <beicker998@gmail.com>
2020-05-25 16:33:41 -0500

    Add some hero styles

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,9 +4,9 @@ const DynamicFlex = (props)=>{
     
     let container = null;
     if(props.flex){
-        container = <div className="flex my-4 max-w-full">{props.children}</div>
+        container = <div className="flex my-2 max-w-full">{props.children}</div>
     }else{
-        container = <div className="block my-4 max-w-full">{props.children}</div>
+        container = <div className="block my-2 max-w-full">{props.children}</div>
     }
 
  
@@ -28,8 +28,8 @@ const InfoParagraph = ({info,textClass = 'text-2xl ml-2', containerClass = 'w-10
 
 const HeroInfo = (props)=>{
     return (
-        <div className="mx-20 w-7/12">
-            <h1 className="text-6xl w-11/12">Estás listo para ser parte de la nueva era digital?</h1>
+        <div className="mx-20 w-8/12">
+            <h1 className="text-6xl font-medium w-full">Estás listo para ser parte de la nueva era digital?</h1>
             <InfoParagraph info="Creamos soluciones en todas las industrias para empresas grandes, pequeñas y startups"/>
         </div>
     )
@@ -37,22 +37,22 @@ const HeroInfo = (props)=>{
 
 const ArrowCta = (props)=>{
 
-    const title = <h4>{props.title}</h4>     
+    const title = <h4 className="mr-5">{props.title}</h4>     
     const titleImg = (
         <React.Fragment>
             {title}
-            <div className="arrow-cta__container ml-auto">
+            <div className="arrow-cta__container ml-auto mr-5">
                 <img className="arrow-cta__arrow-image" src="images/arrow_meet.png" alt="Arrow"/>
             </div>
         </React.Fragment>    
 
     )
-const info = <p className="max-w-full">{props.info}</p>
+const info = <p className="text-sm max-w-full leading-loose">{props.info}</p>
 
 
  
     return (
-        <div className="arrow-cta bg-black text-white w-3/12 ml-auto mr-20 mt-8 pt-5 pb-2 px-5 h-40 rounded-md">
+        <div className="arrow-cta bg-black text-white w-3/12 mr-5 mt-8 pr-2 px-5 h-24 rounded-md">
             <DynamicFlex flex={true}>
                 {titleImg}
             </DynamicFlex>
@@ -64,9 +64,9 @@ const info = <p className="max-w-full">{props.info}</p>
 const Hero = () =>{
  
     return(
-        <section className="h-screen flex pt-32">
+        <section className="h-screen flex pt-24 mb-32">
             <HeroInfo />
-            <ArrowCta  title="CONOZCÁMONOS" info="Solicite una evaluación de digital gratuita y agenda una videollamada"/>
+            <ArrowCta  title="CONOZCÁMONOS" info="Solicite una evaluación digital gratuita y agenda una videollamada"/>
         </section>
     )
 }
