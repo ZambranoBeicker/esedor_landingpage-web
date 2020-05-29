dd1abc64145493deeea6a32046c110a87e09a7cd
Beicker Zambrano <beicker998@gmail.com>
2020-05-28 20:47:18 -0500

    Add responsive styles to brands

--- src/components/Brands.js
+++ src/components/Brands.js
@@ -5,15 +5,15 @@ const BrandImages = ({src, index})=>{
 
 
     return(
-        <div className="w-1/3">
-            <img src={src} alt="brand" className={"brand-" + index}/>
+        <div className="min-w-0 mx-2 my-4 md:w-1/3 relative">
+            <img src={src} alt="brand" className={"brand brand-" + index}/>
         </div>
     )
 }
 
 
 const Brands = ()=>{
-    
+
     const data = [
 
         {
@@ -47,16 +47,33 @@ const Brands = ()=>{
 
     const brands = data.map((img,index) => <BrandImages src={img.src} key={index} index={index}/>)
 
-    return (
-        <section className="flex flex-wrap h-screen mx-10">
-            <div className="flex flex-wrap w-8/12">
+
+    if(window.innerWidth >= 640){
+      return (
+          <section className="md:flex md:flex-wrap h-screen mx-10">
+              <div className="flex flex-wrap md:w-8/12">
+                  {brands}
+              </div>
+              <div className="w-full md:w-3/12 md:ml-auto">
+                  <h2 className="md:text-6xl text-semibig my-10 md:mt-56 md:mb-0">Nuestros aliados</h2>
+              </div>
+          </section>
+      )
+
+    }else{
+
+        return(
+
+          <section className="md:flex md:flex-wrap h-screen mx-4">
+            <div className="w-full my-10 md:w-3/12 md:ml-auto">
+                <h2 className="md:text-6xl text-center text-semibig my-10 md:mt-56 md:mb-0">Nuestros aliados</h2>
+            </div>
+            <div className="flex flex-wrap w-full md:w-8/12">
                 {brands}
             </div>
-            <div className="w-3/12 ml-auto">
-                <h2 className="text-6xl mt-56">Nuestros aliados</h2>
-            </div>
         </section>
-    )
-}
+      )
+    }
+  }
 
 export default Brands;
