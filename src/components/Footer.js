5697dfce895de6a6d001a4a203804700f24c2953
Beicker Zambrano <beicker998@gmail.com>
2020-05-30 16:08:57 -0500

    Add responsive medium styles to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ const FooterInfo = ({pgraph,title}) =>{
 
   return(
         <React.Fragment>
-          <div className="pb-5 pt-4 pl-4 pr-24 md:p-0 border-b md:border-none md:w-3/12 md:mr-16 md:ml-2">
-            <h4 className="text-gray mb-5 ml-auto md:ml-0 w-1/3">{title}</h4>
+          <div className="pb-5 pt-4 pl-4 pr-24 sm:pl-10 md:p-0 border-b md:border-none md:mr-8 md:w-5/12 lg:w-auto md:ml-2">
+            <h4 className="text-gray mb-5 ml-auto md:ml-0 md:w-full lg:w-1/3">{title}</h4>
             {pgraph.map((data,index)=>{
 
               if(!data.src){
@@ -24,7 +24,7 @@ const FooterInfo = ({pgraph,title}) =>{
                 )
             }
             })}
-            
+
           </div>
         </React.Fragment>
   )
@@ -40,11 +40,11 @@ const Footer = () =>{
       title: "Oficina",
       pgraph: [
         {
-          textClass: "text-sm md:text-base w-1/3",
+          textClass: "text-sm md:text-base md:w-full lg:w-1/3",
           text:"Swiss Tower",
         },
         {
-          textClass: "text-sm md:text-base w-9/12",
+          textClass: "text-sm md:text-base md:w-full lg:w-auto",
           text:"Av. Alfredo Benavides #1944 – Piso 9 Miraflores, Lima",
         }
       ],
@@ -66,28 +66,31 @@ const Footer = () =>{
     },
   ]
 
-    if(window.innerWidth >= 960){
+    if(window.innerWidth >= 767){
 
       return (
-        <footer className="py-4 pr-32 border-t">
-          <div className="ml-20 flex w-full">
-            <div>
+        <footer className="md:py-4 border-t py-5 px-6">
+          <div className="xl:ml-20 lg:flex w-full">
+            <div className="md:mb-5 lg:mb-0">
               <img src="images/logo-header.png" alt="Logo"/>
             </div>
-            <div className="flex md:min-w-0 md:w-auto xl:w-8/12 ml-16">
-              {info.map((item,index) => <FooterInfo key={index} title={item.title} pgraph={item.pgraph} /> )}
-              
-            </div>
-            <div className="pb-5 pt-4 px-4 lg:w-3/12 lg:px-10 md:p-0 border-b md:border-none">
-              <h4 className="text-gray mb-5 ml-auto">Sociales</h4>
-              <div className="flex md:block">
-                <p className="mr-4">Linkedin</p>
-                <p className="mr-4">Instagram</p>
-                <p className="">Facebook</p>
+            <div className="flex-wrap flex lg:flex-no-wrap lg:min-w-0">
+
+              <div className="flex min-w-0 md:justify-around md:w-9/12 xl:w-8/12 lg:ml-8">
+                {info.map((item,index) => <FooterInfo key={index} title={item.title} pgraph={item.pgraph} /> )}
+
+              </div>
+              <div className="pb-5 pt-4 px-4 md:w-3/12 lg:w-3/12 lg:px-8 md:p-0 border-b md:border-none">
+                <h4 className="text-gray mb-5 ml-auto">Sociales</h4>
+                <div className="flex md:block">
+                  <p className="mr-4">Linkedin</p>
+                  <p className="mr-4">Instagram</p>
+                  <p className="">Facebook</p>
+                </div>
               </div>
             </div>
           </div>
-          <div className="pl-20 py-4 border-t border-b mb-6 mt-10">
+          <div className=" lg:py-4 border-t border-b mb-6 mt-10">
             <p>© 2018 ESEDOR, todos los derechos reservados.</p>
           </div>
         </footer>
@@ -99,7 +102,7 @@ const Footer = () =>{
           <div className="w-full">
             <div className="w-full md:flex md:flex-wrap">
               {info.map((item,index) => <FooterInfo key={index} title={item.title} pgraph={item.pgraph} /> )}
-              <div className="pb-5 pt-4 px-4 md:p-0 border-b md:w-3/12">
+              <div className="text-sm pb-5 pt-4 px-4 sm:pl-10 md:p-0 border-b md:w-3/12">
                 <h4 className="text-gray mb-5 ml-auto">Sociales</h4>
                 <div className="flex md:block">
                   <p className="mr-4">Linkedin</p>
@@ -109,11 +112,11 @@ const Footer = () =>{
               </div>
             </div>
           </div>
-          <div className="mt-6">
+          <div className="mt-6 sm:pl-10 md:pl-0">
             <img src="images/logo-header.png" alt="Logo"/>
           </div>
-          <div className="text-gray-secondary pl-20 py-4 md:px-0 border-t border-b mb-6 mt-10">
-            <p>© 2018 ESEDOR, todos los derechos reservados.</p>
+          <div className="text-gray-secondary px-4 sm:pl-10 py-4 md:px-0 border-t border-b mb-6 mt-10">
+            <p className="text-sm">© 2018 ESEDOR, todos los derechos reservados.</p>
           </div>
         </footer>
       )
