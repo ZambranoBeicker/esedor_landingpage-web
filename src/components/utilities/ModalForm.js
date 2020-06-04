4cacbdd8871bbce13cba1e6bee9a3705a22b3e44
Beicker Zambrano <beicker998@gmail.com>
2020-06-04 17:52:05 -0500

    Add close icon to modal

--- src/components/utilities/ModalForm.js
+++ src/components/utilities/ModalForm.js
@@ -8,6 +8,9 @@ const ModalForm = ({display})=>{
   const handleClick = useContext(ShowModal)
   return (
         <div className={"inset-0 pt-24 " + display} onClick={()=> handleClick('hidden')}>
+            <div className="modal-image-container cursor-pointer pt-5 pl-5">
+              <img className="modal-image" src="images/close_icon.svg" alt="close" />
+            </div>
             <HeroForm formClass="bg-black min-h-0 sm:mx-4 py-1 px-2 rounded-t-md m-auto" />
         </div>
   )
