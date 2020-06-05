b399470f61495b5fb45a7087867a927926f2d0b9
Beicker Zambrano <beicker998@gmail.com>
2020-06-04 20:15:08 -0500

    Add slider to steps and correct header

--- src/components/Steps.js
+++ src/components/Steps.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Slider from "infinite-react-carousel";
+import Slider from 'infinite-react-carousel';
 import ArrowCta from "./utilities/ArrowCta.js";
 import InfoParagraph from "./utilities/InfoParagraph.js";
 
@@ -8,12 +8,12 @@ const setTitles = ({stepTitle, infoTitle, info},textClass,containerClass,index)=
     return (
             <div className={containerClass} key={index * 7 +2}>
                 <div key={(index * 2 - 3)}>
-                    <h4 className="text-sm xl:text-base font-bold">{stepTitle + ' PASO'}</h4>
+                    <h4 className="mb-5 text-sm xl:text-base font-bold">{stepTitle + ' PASO'}</h4>
                     <h2 className="text-3xl xl:text-4xl w-full">{infoTitle}</h2>
                 </div>
                 <InfoParagraph indexKey={index * (10 - 1 * 3)} info={info} textClass={textClass}/>
 
-                <ArrowCta  title="CONVERSEMOS" containerClass="rounded-md mx-auto md:mx-0 sm:ml-6 my-10 bg-blue py-3 pt-3 px-2 text-shadow " ctaClass="ml-5" src="images/arrow_meet.png" adapt={true}/>
+                <ArrowCta  title="CONVERSEMOS" containerClass="rounded-md mx-auto md:mx-0 sm:ml-6 mt-16 bg-blue py-3 pt-3 px-2 text-shadow " ctaClass="ml-5" src="images/arrow_meet.png" adapt={true}/>
             </div>
     )
 }
@@ -26,7 +26,7 @@ const stepsContent = (data)=>{
         if(steps.orientate){
 
             return(
-                <div key={index * 3} className={"flex mt-40 mb-20 " + steps.containerClass}>
+                <div key={index * 3} className={"flex " + steps.containerClass}>
                     {setTitles(steps,"text-md leading-loose", steps.stepTitle.toLowerCase() + " w-6/12",index)}
                    <div className={steps.imgClass}>
                         <img src={steps.img} alt={steps.stepTitle}/>
@@ -35,7 +35,7 @@ const stepsContent = (data)=>{
                 )
             }else{
             return(
-                <div key={index * 4} className={"relative flex py-20 mt-40 mb-20 " + steps.containerClass}>
+                <div key={index * 4} className={"relative flex py-20 " + steps.containerClass}>
                     <div className={steps.imgClass}>
                         <img src={steps.img} alt={steps.stepTitle}/>
                     </div>
@@ -46,29 +46,44 @@ const stepsContent = (data)=>{
             }
     })
 
-    const contentResponsive = (
+    const contentResponsive = data.map((obj) =>{
 
-        <div className="relative py-20 my-56 ">
+        return (
+
+        <div className="relative py-20 my-56 text-black">
           <div className="mx-auto">
             <div className="flex h-6">
-                <img className="w-10 h-10 rota-180 mx-auto" src="images/steps_arrows.png" alt="steps"/>
-                <h4 className="text-xs w-2/3 pt-3 mx-auto md:text-sm text-center font-bold">{data[0].stepTitle + ' PASO'}</h4>
-                <img className="w-10 h-10 mx-auto" src="images/steps_arrows.png" alt="steps"/>
+                <button className="mx-auto">
+                  <img className="w-10 h-10 rota-180 " src="images/steps_arrows.png" alt="steps"/>
+                </button>
+                <h4 className="text-xs w-2/3 pt-3 mx-auto md:text-sm text-center font-bold">{obj.stepTitle + ' PASO'}</h4>
+                <button className="mx-auto">
+                  <img className="w-10 h-10" src="images/steps_arrows.png" alt="steps"/>
+                </button>
             </div>
-              <img className="w-full my-10 sm:my-16" src={data[0].img} alt={data[0].stepTitle}/>
-              <h2 className=" w-11/12 mx-auto text-semibig sm:mb-4 sm:text-2xl md:text-3xl md:mx-0">{data[0].infoTitle}</h2>
+              <img className="w-full my-10 sm:my-16" src={obj.img} alt={obj.stepTitle}/>
+              <h2 className=" w-11/12 mx-auto text-semibig sm:mb-4 sm:text-2xl md:text-3xl md:mx-0">{obj.infoTitle}</h2>
           </div>
-          <InfoParagraph info={data[0].info} containerClass="'w-11/12 sm:min-w-0 my-3 md:my-5'" textClass="mx-auto md:mx-0 w-11/12 text-xs sm:text-base md:text-lg"/>
+          <InfoParagraph info={obj.info} containerClass="'w-11/12 sm:min-w-0 my-3 md:my-5'" textClass="mx-auto md:mx-0 w-11/12 text-xs sm:text-base md:text-lg"/>
           <ArrowCta  title="CONVERSEMOS" containerClass="rounded-md mx-auto md:mx-0 sm:ml-6 my-10 bg-blue py-3 pt-3 px-2 text-shadow " ctaClass="ml-5" src="images/arrow_meet.png" adapt={true}/>
         </div>
-        )
+        )})
 
 
     if(window.innerWidth >= 1024){
       return content
 
     }else {
-      return contentResponsive
+      return (
+        <Slider
+        arrows={false}
+        autoplay={false}
+
+        >
+          {contentResponsive}
+        </Slider>
+
+          )
     }
 }
 
@@ -81,7 +96,7 @@ const Steps = () =>{
             stepTitle:"PRIMER",
             infoTitle:"Agregamos valor a tu negocio y ofrecemos experiencias excepcionales",
             img:"images/man_top.png",
-            containerClass:"ml-auto w-11/12 py-10 mr-10 lg:mx-auto relative ",
+            containerClass:"mt-40 mb-20 ml-auto w-11/12 py-10 mr-10 lg:mx-auto relative ",
             orientate:true,
             imgClass:"img-primer"
 
@@ -102,7 +117,7 @@ const Steps = () =>{
             infoTitle:"Diseñamos tu nueva tienda virtual y alineamos la estética",
             img:"images/man_bottom.png",
             imgClass:"absolute img-tercer",
-            containerClass:"w-11/12 ml-auto mr-10 relative ",
+            containerClass:"mt-40 mb-20 w-11/12 ml-auto mr-10 relative ",
             orientate:true,
 
         },
@@ -116,7 +131,7 @@ const Steps = () =>{
             infoTitle:"Definimos la tecnologia y empezamos a implementar",
             img:"images/four_step.png",
             containerClass:"w-full py-20 pl-20 bg-black text-white",
-            imgClass:"block h-screen w-5/12 ml-auto",
+            imgClass:"block h-screen pt-10 w-5/12 ml-auto",
             orientate:true,
         },
         {
@@ -126,7 +141,7 @@ const Steps = () =>{
             infoTitle:"Lanzamos al mercado tu producto digital y optimizamos el rendimiento",
             img:"images/fifth_step.png",
             imgClass:"block h-screen w-6/12 ml-auto",
-            containerClass:"w-full py-20 pl-20 bg-black text-white",
+            containerClass:"w-full pb-20 pl-20 bg-black text-white",
             orientate:true,
         }
     ]
@@ -135,31 +150,11 @@ const Steps = () =>{
     return(
         <section id="steps" className="min-h-0 md:px-16 lg:px-0">
             {stepsContent(data)}
-            <Slider
-            className="bg-black pt-2"
-            duration={200}
-            autoplay={true}
-            dotsScroll={2}
-            arrows={false}
-            autoplaySpeed={4000}
-            >
-            <div className={"flex " + lastData[0].containerClass}>
-                <div className="flex">
-                    {setTitles(lastData[0],"text-md leading-loose lg:mb-24", lastData[0].stepTitle.toLowerCase() + " w-6/12",(15))}
-                    <div className={lastData[0].imgClass}>
-                        <img src={lastData[0].img} alt={lastData[0].stepTitle}/>
-                    </div>
-                </div>
+
+            <div className="hidden">
+
+            {stepsContent(lastData)}
             </div>
-            <div className={"flex " + lastData[1].containerClass}>
-                <div className="flex">
-                    {setTitles(lastData[1],"text-md leading-loose lg:mb-24", lastData[1].stepTitle.toLowerCase() + " w-6/12",(14))}
-                    <div className={lastData[1].imgClass}>
-                        <img src={lastData[1].img} alt={lastData[1].stepTitle}/>
-                    </div>
-                </div>
-            </div>
-            </Slider>
         </section>
     )
 }
