4e2f501a7167bd1bd6bdd4f7101bd3de928ff985
Beicker Zambrano <beicker998@gmail.com>
2020-06-11 17:39:53 -0500

    Correct bugs #12

--- src/components/utilities/ModalForm.js
+++ src/components/utilities/ModalForm.js
@@ -14,12 +14,12 @@ const ModalForm = ({display, displayDiv})=>{
             click.app('')
           }}></div>
           <div className="max-w-sm mx-auto">
-            <div className="modal-image-container cursor-pointer md:pt-5 md:pl-5 circle-background" onClick={()=>{
+            <div className="modal-image-container cursor-pointer md:pt-5 md:pl-5" onClick={()=>{
             click.active('hide')
             click.activeDiv('hidden')
             click.app('')
           }}>
-              <img className="modal-image" src="images/close_icon.svg" alt="close" />
+              <img className="modal-image" src="images/close-icon.svg" alt="close" />
             </div>
             <HeroForm containerForm="form-container" formClass="bg-black min-h-0 py-1 px-2 rounded-t-md m-auto" />
           </div>
