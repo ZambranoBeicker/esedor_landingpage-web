2cb4940951fb243531c42cd017935544434d1c44
Beicker Zambrano <beicker998@gmail.com>
2020-06-23 17:42:21 -0500

    Correcting little details on hero

--- src/components/utilities/HeroInfo.js
+++ src/components/utilities/HeroInfo.js
@@ -4,7 +4,7 @@ import ArrowCta  from "./ArrowCta";
 
 const HeroInfo = (props)=>{
     return (
-        <div className="mx-auto max-w-xs sm:max-w-none px-5 hero-info--max-370-p-0 relative my-index sm:w-6/12 md:ml-10 md:mr-10 lg:ml-16 md:w-9/12 lg:w-7/12 sm:pr-10 sm:pl-2 lg:pr-40 hero-info--big-desktop">
+        <div className="mx-auto max-w-xs sm:max-w-none px-5 hero-info--max-370-p-0 relative my-index sm:w-6/12 md:ml-10 md:mr-10 lg:ml-16 sm:pr-10 sm:pl-2 hero-info--big-desktop">
             <h1 className="text-2xl font-semibold sm:mb-4 lg:mt-32 lg:mb-8 sm:text-3xl lg:text-5xl sm:font-medium w-full">Estás listo para vender por tu tienda virtual?</h1>
             <InfoParagraph info="Creamos soluciones ecommerce en todas las industrias para  empresas grandes, pequeñas y startups."/>
             <ArrowCta  title="Llámanos por" containerClass="rounded-md hidden lg:flex lg:w-32 mx-auto md:mx-0 sm:ml-6 my-10 bg-blue text-shadow " ctaClass="mr-auto" adapt={false} route="llamada"/>
